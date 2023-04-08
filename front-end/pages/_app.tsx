ecdcbc77145922a9ff8d54f70507e096c40db87f
Bao Khanh <tlbk1999@gmail.com>
2023-04-08 17:53:12 +0700

    Add layout

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://kit.fontawesome.com/11a9b329dc.js"
+        crossOrigin="anonymous"
+      ></Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
