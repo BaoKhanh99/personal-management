6bee1e9dcb1325c7f3fd632659af1549638b755c
Bao Khanh <tlbk1999@gmail.com>
2024-04-28 21:27:35 +0700

    [FE] use flowbite-react

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
